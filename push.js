1617f871cb9429c08552cd9683f7717bdc330d61
Constantin Guiu <guiuconstantin@gmail.com>
2021-07-23 12:46:11 +0200

    files added

---
+++ push.js
@@ -0,0 +1,12 @@
+var push = require('web-push')
+
+let vapIDKeys = {
+    publicKey: 'BFagtQKIJv3RTemYCIivpH2u5w0iQweXErrxBcfpgElaDofdd7yQS90mU598OcIcdikzyZbdf7FZtUTDWMRfG4s',
+    privateKey: '3s7YVeW5-OkYvpnfLUk5aSW8OitsRCBaEvcILZfIN2k'
+}
+
+push.setVapidDetails('mailto:guiu_costin_2009@yahoo.com', vapIDKeys.publicKey, vapIDKeys.privateKey)
+
+let sub = {}
+
+push.sendNotification(sub, 'test message')
