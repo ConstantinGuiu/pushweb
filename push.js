344bf99f02f9aaafe6e6b4e18e807cdd9f7d0c34
Constantin Guiu <guiuconstantin@gmail.com>
2021-07-26 10:38:52 +0200

    endpoint link fix

--- push.js
+++ push.js
@@ -14,6 +14,6 @@ push.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = { endpoint: "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABg_…WXyrRNzf4YKBNLLPai6mekmO4pmgrGb5-Tp0dHty8g6V0RQP_p09y9fCzLZA", keys: { auth: "pAe2RsQ1AIfATetqTjLhkw", p256dh: "BNTOasixwYg4Y2714zn8b3Oye45RoTRVOWNR0P1gFKSVdbP9hS7TqBdcm1ubtUt_Hxv6LYXBKk_0jWC_dMKBit0" } };
+const pushSubscription = { endpoint: "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABg_mfyEK-Cj8cAW5t84-QZ769JgJJCtyNscYnG68Z9vFjN8gvqSlEe1BJEDUVdJeByvzcNfJkI_dPxtchFO-MGvZaM4k1pY76bTlA1xitY3SZscx7gl-SWXyrRNzf4YKBNLLPai6mekmO4pmgrGb5-Tp0dHty8g6V0RQP_p09y9fCzLZA", keys: { auth: "pAe2RsQ1AIfATetqTjLhkw", p256dh: "BNTOasixwYg4Y2714zn8b3Oye45RoTRVOWNR0P1gFKSVdbP9hS7TqBdcm1ubtUt_Hxv6LYXBKk_0jWC_dMKBit0" } };
 
 push.sendNotification(pushSubscription, 'Your Push Payload Text');
